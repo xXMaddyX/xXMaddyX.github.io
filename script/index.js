5c7705dafb46885c146e8938ef6db91afd3bd17f
Sven Höhn <126699113+xXMaddyX@users.noreply.github.com>
2023-12-23 02:47:36 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,1 +1,29 @@
+const link1 = document.getElementById("list1");
+const link2 = document.getElementById("list2");
+const link3 = document.getElementById("list3");
 
+const listAnimationText = {
+    text1: "Projekte",
+    text2: "Info",
+    text3: "Kontakt",
+}
+
+
+const linkAnimation = async (listAnimationText, element) => {
+    return new Promise((resolve) => {
+        for (let i = 0; i < listAnimationText.length; i++) {
+            setTimeout(() => {
+                element.textContent += listAnimationText[i]
+                if (i === listAnimationText.length - 1) resolve();
+            },i * 100)
+        }
+    });
+}
+
+const executAnimation = async () => {
+    await linkAnimation(listAnimationText.text1, link1)
+    await linkAnimation(listAnimationText.text2, link2)
+    await linkAnimation(listAnimationText.text3, link3)
+}
+
+executAnimation()

a93e341782ce449e4f395e7e367a1ced6dc2c6c2
xXMaddyX <maddyaion@hotmail.de>
2024-01-15 13:19:37 +0100

    update 15.01.

--- script/textStore.js
+++ script/textStore.js
@@ -4,11 +4,16 @@ export const textData = [
     imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV87jhBlsqDmcQxHI6AEMhjd5Jf5I7UKTXd_uHAqw9P6fWYEPPw4BtMwH0SXCD0pP7bcdfLf2AZdGLFYTb8Cz8LsYcqnhqzqv8oO0y7B8d3tqBxCq5ApPtFMz0rLjh5Yu6cPc98XqLG5TKuj2XCZ1L0hV=w918-h761-s-no-gm?authuser=0",
     projektLink: "https://www.bibelgame.de/"},
 
+    {projectHeader: "Piep´s ChatRoom",
+    text: "Eine kleine WetterApp erstellt mit Electron.js",
+    imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV86v3xzp-gW1xEbEoOvISbwVJyhRr-wFy-gIChxQ8Z78IHR2co0nk6OdMBn2OMFU_4hfDxbd2c1gnFF14JdIEK6uCiaZmmjGwVT0aZtfEXvCXiqmumJFqgtgtB6CsCUrnqw4lsSuTs67bwuufX2IaZUM=w996-h761-s-no-gm?authuser=0",
+    projektLink: "https://github.com/xXMaddyX/PiepsChatSystem-Rooms-"},
+    
     {projectHeader: "The Sun",
     text: "ein Kleines Demo Projekt (Work in Progress)",
     imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV87M2QoAlOeUA92w_npQXI0z4QI4D1Pf3vCyMgC--e8dTMrKSQVDYrUbxARNtygGK7DXZxu98wxNNagXl9_Za2R8b1Mz5WaF5-x4cIy3c0AuEzfGoyyT3QUhnTUyED0PTKAkPHQ6o0TlXCmToHEVrAhv=w1185-h825-s-no-gm?authuser=0",
     projektLink: "../demoprojects/TheSun/sonne.html"},
-
+    
     {projectHeader: "3D Moddeling",
     text: "3D Icon Moddeling in Blender und Babylon.js ",
     imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV87wcr9nsJii1EfWInloIphLvoaujYQxBX2YiQDQw6bi4YcfgjfmG8O5BFVGlyEAFe6Ho81ruEFT7QDsJIypB-s1EtGvJb9Guklv5AqKOvxPenT6MDxLuNzUBgG1U8kqarqtbeeq-3IdLGN6aBEoAiJL=w1024-h678-s-no-gm?authuser=0",
@@ -19,25 +24,21 @@ export const textData = [
     imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV86fRRAmxSmr1IpYqn_7gnjTvBy84TS65LegULD6vJxpO4MlMPA8OwQi2jWRgooma80ShqoUm4hQY9LBfv2ZI9lamo4B5aX_XHXR0moIP6jvL1eV1dipGYfg5pAHGfVqXaNt2PWQWG5ZaoTanIvR80tR=w598-h356-s-no-gm?authuser=0",
     projektLink: "https://github.com/xXMaddyX/PiepsLager"},
 
-    {projectHeader: "Kleine WetterApp",
-    text: "Eine kleine WetterApp erstellt mit Electron.js",
-    imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV86R3skEPLRNPUmjnilIjtIMBtNL3IzcRXi2vlR-EZEPfHVO0R9GrYQ34PnmeuRk3OavWZQbVhqvWIzYq2d0Ghn9XCuPcsCF_EtdS0J8QRAdjdElzYEDHHUIJCStuHybCw74IbveVRWpPntyO2sMuMcn=w598-h481-s-no-gm?authuser=0",
-    projektLink: "https://github.com/xXMaddyX/WetterappForElectron"},
-
+    {projectHeader: "FreelanceArchery Datenbank",
+    text: "Eine Bogensport Datenbank für Bögen und Pfeile (Work in Progress)",
+    imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV85qN3ka2y_Ca4tnMpv3fF5fqiVI98dmt1SbF4FU4HH8j5ZFM1wq_KPWJN1-rFt05UjjbXAJXKgT6XftNGOAnGYEsKU-k9jIK-LaOZIOAlNqXz7ZQ2zVtpWilHhv6Q202ycWkVtmGUJYTCYALMNjKxX3=w973-h577-s-no-gm?authuser=0",
+    projektLink: "https://github.com/xXMaddyX/FreelanceDatabase"},
+    
     {projectHeader: "Piep´s MusicPlayer",
     text: "Ein Kleiner Desktop MP3 Player mit Electron,Vue, HTML, CSS und JS erstellt.",
     imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV868R8DGyxdG3vjjf2dF5K_ZR3v9i9jeClJtKLTqTexD0lVkl2UQTa6FmTg1GZ1zX8Qc5Tr3UHIC4wSreqc8tQe700PLFiyJpDIjx84WvwUrnn1ZZZUUr_Jh1oPthmUGzeHIO2t4LkisTZl4W8t0QH1g=w598-h412-s-no-gm?authuser=0",
     projektLink: "https://github.com/xXMaddyX/SimpelPiepsMP3Player"},
 
-    {projectHeader: "FreelanceArchery Datenbank",
-    text: "Eine Bogensport Datenbank für Bögen und Pfeile",
-    imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV85qN3ka2y_Ca4tnMpv3fF5fqiVI98dmt1SbF4FU4HH8j5ZFM1wq_KPWJN1-rFt05UjjbXAJXKgT6XftNGOAnGYEsKU-k9jIK-LaOZIOAlNqXz7ZQ2zVtpWilHhv6Q202ycWkVtmGUJYTCYALMNjKxX3=w973-h577-s-no-gm?authuser=0",
-    projektLink: "https://github.com/xXMaddyX/FreelanceDatabase"},
-
-    {projectHeader: "Piep´s ChatRoom",
+    {projectHeader: "Kleine WetterApp",
     text: "Eine kleine WetterApp erstellt mit Electron.js",
-    imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV86v3xzp-gW1xEbEoOvISbwVJyhRr-wFy-gIChxQ8Z78IHR2co0nk6OdMBn2OMFU_4hfDxbd2c1gnFF14JdIEK6uCiaZmmjGwVT0aZtfEXvCXiqmumJFqgtgtB6CsCUrnqw4lsSuTs67bwuufX2IaZUM=w996-h761-s-no-gm?authuser=0",
-    projektLink: "https://github.com/xXMaddyX/PiepsChatSystem-Rooms-"},
+    imgSrc: "https://lh3.googleusercontent.com/pw/ABLVV86R3skEPLRNPUmjnilIjtIMBtNL3IzcRXi2vlR-EZEPfHVO0R9GrYQ34PnmeuRk3OavWZQbVhqvWIzYq2d0Ghn9XCuPcsCF_EtdS0J8QRAdjdElzYEDHHUIJCStuHybCw74IbveVRWpPntyO2sMuMcn=w598-h481-s-no-gm?authuser=0",
+    projektLink: "https://github.com/xXMaddyX/WetterappForElectron"},
+
 
 ]
 
@@ -47,9 +48,10 @@ export const infoText = {
         fotoHeading: `Infos Über Mich`,
         fotoText: [
             {text: `Hallo und herzlich willkommen!`},
-            {text: `Mein Name ist Sven und ich bin Web und Grafik Designer.`},
+            {text: `Mein Name ist Sven und ich bin Web-Entwickler.`},
             {text: `Seit anfang 2023 beschäftige ich mich mit Web und Grafik Entwicklung, und es hat mir eine Welt voller Herausforderungen, Entdeckungen und unendlicher Möglichkeiten eröffnet.`},
-            {text: `Ich habe meine Karriere neben meiner Selbstständigkeit begonnen und konnte seitdem zahlreiche spannende Projekte realisieren. Mein Spezialgebiet liegt in der Web und Backend Entwicklung, und ich genieße es, innovative Lösungen für komplexe Probleme zu finden. Neben meiner Selbstständigkeit und Webentwicklung bin ich auch im Bogensport unterwegs.`}
+            {text: `Seitdem konnte ich einige spannende Projekte realisieren. Meine Leidenschaft habe ich in der Webentwicklung und dem Grafikdesign gefunden. Im Grafikbereich Arbeite ich mit Blender, UT5 und Babylon.js`},
+            {text: `Neben meiner Selbstständigkeit und Webentwicklung bin ich auch Hobbymäßig im Feld-Bogensport unterwegs.`}
         ]
     },
     mainTextSection: {
